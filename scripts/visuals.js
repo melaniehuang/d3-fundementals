a0e0c9bf9ade85e92d1e20b74a78e76362f66d8f
Mel Huang <hello@melaniehuang.com>
2016-11-26 17:55:20 +1100

    adding titles

--- scripts/visuals.js
+++ scripts/visuals.js
@@ -18,35 +18,62 @@ window.onload = function(){
     }
   ];
 
-  var w = 500;
-  var h = 500;
+  var w = 1000;
+  var h = 300;
+  
+  d3.select("body").append("p")
+  				   .text("HIGH FODS")
+  				   .style("color", "white");
 
-   var svg = d3.select("body").append("svg");
+  var svg = d3.select("body")
+   			  .append("svg")
+              .attr("width", w)
+              .attr("height", h);
 
-   svg.attr("width", w)
-      .attr("height", h);
+  var rects = svg.selectAll("rect")
+				 .data(fruits)
+				 .enter()
+				 .append("rect");
 
-   var circles = svg.selectAll("circle")
-				    .data(fruits)
-				    .enter()
-				    .append("circle");
+  var text = svg.selectAll("text")
+                .data(fruits)
+                .enter()
+                .append("text")
+                .text(function(d){
+                  return d;
+                })
+                .attr("x",
+		          function(d, i) {
+			       return (i * 30) + 10;
+			      })
+   				.attr("y", function(d) {
+        		   return h - (d * 4);
+   				});
 
-   circles.attr("cx", 
-     function(d, i) {
-       return (i * 100) + 70;
-     })
-     .attr("cy", function(d, i){
-       if (fruits[i].fodmap == "high"){
-         return h/2 - 50;
-       } else if (fruits[i].fodmap == "low"){
-       	 return h/2 + 50;
-       }
-     }) 	
-     .attr("r", 20)
-     .attr("fill", function(d, i) {
-     	return fruits[i].color;
-     })
-     ;
+  rects.attr("x",
+         function(d, i) {
+	       return (i * 30) + 10;
+	     })
+	   .attr("y", function(d, i){
+	     if (fruits[i].fodmap == "high"){
+	       return 30;
+	     } else if (fruits[i].fodmap == "low"){
+	   	   return 150;
+	     }
+	   })
+	   .attr("width", 10)
+	   .attr("height", 100)
+	   .attr("fill", function(d, i) {
+	 	  return fruits[i].color;
+	   }
+	   .attr(function(d, i) {
+         return fruits[i].kind;
+       })
+  );
+	 
+  d3.select("body").append("p")
+				   .text("LOW FODS")
+				   .style("color", "white");
 
 }
 
