100277a1d586e52b3f8d5f6eee552e24746c0dc9
Mel Huang <hello@melaniehuang.com>
2016-11-26 16:01:19 +1100

    exploring svg style and json

--- scripts/visuals.js
+++ scripts/visuals.js
@@ -1,19 +1,52 @@
-window.onload = function(){
-  var dataset = []; 
 
-  for (var i = 0; i < 12; i++) {           
-    var newNumber = Math.round(Math.random() * 30);
-    dataset.push(newNumber);
-  }             
-  
-  d3.select("body").selectAll("p")
-    .data(dataset)
-    .enter()
-    .append("div")
-    .attr("class", "bar")
-    .style("height", function(d){
-    	return d*20 + "px";
-      }
-    );
+window.onload = function(){
+  var fruits = [
+    {
+        kind: "blueberry",
+        color: "blueviolet",
+        fodmap: "low"
+    },
+    {
+        kind: "apricot",
+        color: "coral",
+        fodmap: "high"
+    },
+    {
+        kind: "banana",
+        color: "yellow",
+        fodmap: "low"
+    }
+  ];
+
+  var w = 500;
+  var h = 500;
+
+   var svg = d3.select("body").append("svg");
+
+   svg.attr("width", w)
+      .attr("height", h);
+
+   var circles = svg.selectAll("circle")
+				    .data(fruits)
+				    .enter()
+				    .append("circle");
+
+   circles.attr("cx", 
+     function(d, i) {
+       return (i * 100) + 70;
+     })
+     .attr("cy", function(d, i){
+       if (fruits[i].fodmap == "high"){
+         return h/2 - 50;
+       } else if (fruits[i].fodmap == "low"){
+       	 return h/2 + 50;
+       }
+     }) 	
+     .attr("r", 20)
+     .attr("fill", function(d, i) {
+     	return fruits[i].color;
+     })
+     ;
+
 }
 
