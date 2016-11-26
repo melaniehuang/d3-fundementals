a1fb8decf35d09e6bced7b7d124dae1af6997b4f
Mel Huang <hello@melaniehuang.com>
2016-11-26 12:19:13 +1100

    getting into svgs

--- scripts/visuals.js
+++ scripts/visuals.js
@@ -1,4 +1,19 @@
 window.onload = function(){
-  var dataset = [ 5, 10, 15, 20, 25 ];
-  d3.select("body").append("p").text("New paragraph!");
+  var dataset = []; 
+
+  for (var i = 0; i < 12; i++) {           
+    var newNumber = Math.round(Math.random() * 30);
+    dataset.push(newNumber);
+  }             
+  
+  d3.select("body").selectAll("p")
+    .data(dataset)
+    .enter()
+    .append("div")
+    .attr("class", "bar")
+    .style("height", function(d){
+    	return d*20 + "px";
+      }
+    );
 }
+
